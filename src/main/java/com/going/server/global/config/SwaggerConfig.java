50510be20251e9f32c646d1ee4d8d079b51d04cd
hyeonda02 <alceste951005@naver.com>
2025-01-30 12:40:40 +0900

    🔧 Fix: cors configuration 수정

--- src/main/java/com/going/server/global/config/SwaggerConfig.java
+++ src/main/java/com/going/server/global/config/SwaggerConfig.java
@@ -5,6 +5,8 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SwaggerConfig {
@@ -17,5 +19,18 @@ public class SwaggerConfig {
 
         return new OpenAPI()
             .info(info);
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
