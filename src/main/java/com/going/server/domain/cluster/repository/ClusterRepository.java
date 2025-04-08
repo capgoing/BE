fcad5b04b0a8595aa8ddb18ec55f784d90ce587c
DaHyeon <113489721+hyeonda02@users.noreply.github.com>
2025-04-09 01:03:44 +0900

    ♻️ [Refactor] Node 구조 변경 및 Repository 메소드명 변경 (#45)

--- src/main/java/com/going/server/domain/cluster/repository/ClusterRepository.java
+++ src/main/java/com/going/server/domain/cluster/repository/ClusterRepository.java
@@ -5,11 +5,15 @@ import com.going.server.domain.cluster.exception.ClusterNotFoundException;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ClusterRepository extends Neo4jRepository<Cluster, Long> {
     Cluster findByRepresentWord(String word);
 
+    Optional<Cluster> findByClusterId(Long clusterId);
+
     default Cluster getByCluster(Long clusterId) {
-        return findById(clusterId).orElseThrow(ClusterNotFoundException::new);
+        return findByClusterId(clusterId).orElseThrow(ClusterNotFoundException::new);
     }
 }
--- src/main/java/com/going/server/domain/word/repository/WordRepository.java
+++ src/main/java/com/going/server/domain/word/repository/WordRepository.java
@@ -6,12 +6,15 @@ import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface WordRepository extends Neo4jRepository<Word, Long> {
+    Optional<Word> findByWordId(Long wordId);
+
     List<Word> findByCluster_ClusterId(Long clusterId);
 
     default Word getByWord(Long wordId) {
-        return findById(wordId).orElseThrow(WordNotFoundException::new);
+        return findByWordId(wordId).orElseThrow(WordNotFoundException::new);
     }
 }
