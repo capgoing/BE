87b87cd1b6500387ce93da8dfed73c41fa24b6de
Juye0nLee <dlwndus708@naver.com>
2025-04-10 15:07:38 +0900

    🔨 [setting]지식그래피 조회 api 구조 셋팅

--- src/main/java/com/going/server/domain/graph/controller/graphController.java
+++ src/main/java/com/going/server/domain/graph/controller/graphController.java
@@ -2,6 +2,7 @@ package com.going.server.domain.graph.controller;
 
 import com.going.server.domain.graph.dto.graphDto;
 import com.going.server.domain.graph.dto.graphListDto;
+import com.going.server.domain.graph.dto.knowledgeGraphDto;
 import com.going.server.domain.graph.service.graphService;
 import com.going.server.global.response.SuccessResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -52,6 +53,23 @@ public class graphController {
     public SuccessResponse<?> deleteGraph(@PathVariable("graphId") Long graphId) {
         graphService.deleteGraph(graphId);
         return SuccessResponse.empty();
+    }
+
+    @GetMapping("/{graphId}")
+    @Operation(summary = "지식 그래프 전체 데이터(전체 화면) 조회", description = "현재 저장된 전체 지식 그래프 데이터를 조회합니다.")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "지식 그래프가 성공적으로 조회되었습니다.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(example = "{\"message\":\"\"}")
+                    )
+            )
+    })
+    public SuccessResponse<knowledgeGraphDto> getGraph(@PathVariable("graphId") Long graphId) {
+        knowledgeGraphDto result = graphService.getGraph(graphId);
+        return SuccessResponse.of(result);
     }
 
 }
--- src/main/java/com/going/server/domain/graph/service/graphService.java
+++ src/main/java/com/going/server/domain/graph/service/graphService.java
@@ -2,8 +2,11 @@ package com.going.server.domain.graph.service;
 
 import com.going.server.domain.graph.dto.graphDto;
 import com.going.server.domain.graph.dto.graphListDto;
+import com.going.server.domain.graph.dto.knowledgeGraphDto;
 
 public interface graphService {
     graphListDto getGraphList();
     void deleteGraph(Long graphId);
+
+    knowledgeGraphDto getGraph(Long graphId);
 }
