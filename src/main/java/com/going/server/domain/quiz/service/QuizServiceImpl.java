2217bb8d7760e759a3ae71e5ae67600df17cf0b8
khyaejin <olivia019182@naver.com>
2025-04-17 03:08:24 +0900

    ✨ [feat] 퀴즈 생성 API 구조 세팅 - service TODO 주석 추가 (listenUp)
    service 계층 코드에 필요한 로직 DOTO 주석으로 추가

--- src/main/java/com/going/server/domain/quiz/service/QuizServiceImpl.java
+++ src/main/java/com/going/server/domain/quiz/service/QuizServiceImpl.java
@@ -7,6 +7,11 @@ public class QuizServiceImpl implements QuizService{
 
     @Override
     public QuizCreateResponseDto QuizCreate(String mode, String graphId) {
+        // TODO : mode별 분기 필요
+
+        // TODO : graphId로 지식그래프 예시 문장 3개 랜덤으로 가져오기
+
+        // TODO : 각 문장을 반환 형식에 맞게 가공
         return null;
     }
 }
