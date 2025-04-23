65e2b3d7b37b000b8fa38e37e5f9b66bd066fdb2
khyaejin <olivia019182@naver.com>
2025-04-23 21:16:12 +0900

    ✨ [feat] 퀴즈 생성 API -  DTO 생성 및 전체적인 서비스로직 구현 완료

--- src/main/java/com/going/server/domain/quiz/dto/QuizCreateResponseDto.java
+++ src/main/java/com/going/server/domain/quiz/dto/QuizCreateResponseDto.java
@@ -1,9 +1,16 @@
 package com.going.server.domain.quiz.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.stereotype.Service;
 
 // 퀴즈 생성 반환 DTO
-public class QuizCreateResponseDto {
+@Getter
+@Service
+@AllArgsConstructor
+public class QuizCreateResponseDto<T> {
     private String graphId;
     private String mode; // listenUp, connect, picture
 
