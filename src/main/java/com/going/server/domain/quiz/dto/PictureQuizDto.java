b78e251098cfb9981df97935b2d5f1586c204bc8
khyaejin <olivia019182@naver.com>
2025-04-27 18:44:30 +0900

    ✨ [feat] PictureQuizDto 생성

--- src/main/java/com/going/server/domain/quiz/dto/PictureQuizDto.java
+++ src/main/java/com/going/server/domain/quiz/dto/PictureQuizDto.java
@@ -3,7 +3,12 @@ package com.going.server.domain.quiz.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.List;
+
 @Builder
 @Getter
 public class PictureQuizDto {
+    private final String imageUrl;
+    private final List<String> shuffled;
+    private final String answer;
 }
--- src/main/java/com/going/server/domain/quiz/generate/PictureQuizGenerator.java
+++ src/main/java/com/going/server/domain/quiz/generate/PictureQuizGenerator.java
@@ -1,16 +1,25 @@
 package com.going.server.domain.quiz.generate;
 
 import com.going.server.domain.graph.entity.Graph;
+import com.going.server.domain.graph.entity.GraphNode;
+import com.going.server.domain.quiz.dto.ListenUpQuizDto;
 import com.going.server.domain.quiz.dto.PictureQuizDto;
 import org.springframework.stereotype.Component;
+
+import java.util.*;
 
 @Component
 public class PictureQuizGenerator implements QuizGenerator<PictureQuizDto> {
 
     @Override
     public PictureQuizDto generate(Graph graph) {
-        // TODO: picture 퀴즈 생성 로직
+        Set<String> shuffled = new HashSet<>();
+        String answer;
 
-        return null;
+        return PictureQuizDto.builder()
+                .imageUrl("https://s3-example")
+                .shuffledOptions(shuffled)
+                .answer(answer)
+                .build();
     }
 }
