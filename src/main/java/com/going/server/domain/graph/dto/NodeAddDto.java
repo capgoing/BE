3380137161873a69a11a8d8f7ea1bdd07578b093
Juye0nLee <dlwndus708@naver.com>
2025-04-29 00:49:27 +0900

    ➕ [chore] validation 의존성 추가

--- src/main/java/com/going/server/domain/graph/dto/NodeAddDto.java
+++ src/main/java/com/going/server/domain/graph/dto/NodeAddDto.java
@@ -1,19 +1,19 @@
 package com.going.server.domain.graph.dto;
 
-import jakarta.validation.constraints.NotNull;
-import lombok.Builder;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class NodeAddDto {
-    @NotNull(message = "추가할 부모 노드의 ID를 입력해주세요.")
+    @NotBlank(message = "추가할 부모 노드의 ID를 입력해주세요.")
     String parentId;
-    @NotNull(message = "추가할 라벨(nodeLabel)을 입력해주세요.")
+    @NotBlank(message = "추가할 라벨(nodeLabel)을 입력해주세요.")
     String nodeLabel;
-    @NotNull(message = "추가할 노드의 관계(edgeLabel)를 입력해주세요.")
+    @NotBlank(message = "추가할 노드의 관계(edgeLabel)를 입력해주세요.")
     String edgeLabel;
-    @NotNull(message = "문장을 입력해주세요")
+    @NotBlank(message = "문장을 입력해주세요")
     String includeSentence;
 }
