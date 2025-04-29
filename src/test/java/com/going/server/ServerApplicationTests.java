45eed15c6da8660c1015cc3f8a0b69e53f2df425
hyeonda02 <alceste951005@naver.com>
2025-04-29 22:37:53 +0900

    🐛 [fix] CI에 test 제거

--- src/test/java/com/going/server/ServerApplicationTests.java
+++ src/test/java/com/going/server/ServerApplicationTests.java
@@ -1,9 +1,10 @@
 package com.going.server;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@WebMvcTest
 class ServerApplicationTests {
 
 	@Test
