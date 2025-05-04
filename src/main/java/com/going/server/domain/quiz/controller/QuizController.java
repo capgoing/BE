21f11f70d635dca10c142a3f47d0fb48da738aee
khyaejin <olivia019182@naver.com>
2025-05-04 13:02:08 +0900

    ✨ [feat] 퀴즈 결과 저장 api 구현 - controller 로직 구현

--- src/main/java/com/going/server/domain/quiz/controller/QuizController.java
+++ src/main/java/com/going/server/domain/quiz/controller/QuizController.java
@@ -1,6 +1,7 @@
 package com.going.server.domain.quiz.controller;
 
 import com.going.server.domain.quiz.dto.QuizCreateResponseDto;
+import com.going.server.domain.quiz.dto.QuizSavedRequestDto;
 import com.going.server.domain.quiz.service.QuizService;
 import com.going.server.domain.quiz.service.QuizServiceImpl;
 import com.going.server.global.response.SuccessResponse;
@@ -16,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/quiz")
 @RequiredArgsConstructor
-@Tag(name="[캡스톤]ListenUpQuiz", description = "퀴즈 관련 통신을 위한 API")
+@Tag(name="[캡스톤]Quiz", description = "퀴즈 관련 통신을 위한 API")
 public class QuizController {
 
     private final QuizService quizService;
@@ -36,6 +37,23 @@ public class QuizController {
     public SuccessResponse<?> createQuiz(@PathVariable String graphId, @RequestParam String mode) {
         QuizCreateResponseDto result = quizService.quizCreate(graphId, mode);
         return SuccessResponse.of(result, "201");
+    }
+
+    @PatchMapping("perfect-score/{graphId}")
+    @Operation(summary = "[퀴즈화면] 퀴즈 결과 저장", description = "퀴즈진행 후 해당 모드의 퀴즈 결과를 저장합니다..")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "퀴즈를 성공적으로 저장하였습니다.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(example = "{\"message\":\"\"}")
+                    )
+            )
+    })
+    public SuccessResponse<?> reportPerfectScore(@PathVariable String graphId, @RequestParam String mode) {
+        quizService.updateIfPerfect(graphId, mode);
+        return SuccessResponse.of(null, "200");
     }
 
 }
--- src/main/java/com/going/server/domain/quiz/service/QuizService.java
+++ src/main/java/com/going/server/domain/quiz/service/QuizService.java
@@ -7,4 +7,7 @@ public interface QuizService {
 
     // 모드 별 퀴즈 생성
     public QuizCreateResponseDto quizCreate(String graphId, String mode);
+
+    // 만점일 경우 Graph Quiz 정보 업데이트
+    void updateIfPerfect(String graphId, String mode);
 }
