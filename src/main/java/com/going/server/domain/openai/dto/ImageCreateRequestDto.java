e61a1667eb15ae4f926fc4cdd8cb387dc816dc32
khyaejin <olivia019182@naver.com>
2025-05-25 20:52:43 +0900

    ⚡️ [perf] picture 퀴즈 이미지 생성 결과 개선

--- src/main/java/com/going/server/domain/openai/dto/ImageCreateRequestDto.java
+++ src/main/java/com/going/server/domain/openai/dto/ImageCreateRequestDto.java
@@ -3,13 +3,20 @@ package com.going.server.domain.openai.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ImageCreateRequestDto {
     private String prompt;
     private String model = "dall-e-3";
-    private String size = "1024x1024"; // 기본값으로 지정
-    private int n = 1; // 기본값 넣어두기
+    private String style = "vivid";
+    private String size = "1024x1024";
+    private int n = 1;
+
+    public ImageCreateRequestDto(String prompt) {
+        this.prompt = prompt;
+    }
 }
