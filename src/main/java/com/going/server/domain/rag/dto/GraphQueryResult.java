6414288aaeef9a4a75f81abf38e4d06dd4b585a1
khyaejin <olivia019182@naver.com>
2025-05-29 10:29:23 +0900

    ⚡️ [Perf] GraphRAG - GraphQueryResult 모양 변경

--- src/main/java/com/going/server/domain/rag/dto/GraphQueryResult.java
+++ src/main/java/com/going/server/domain/rag/dto/GraphQueryResult.java
@@ -6,6 +6,13 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class GraphQueryResult {
+    private String sourceLabel;
+    private String relationLabel;
+    private String targetLabel;
     private String sentence;
     private String nodeLabel;
+
+    public String toTripleString() {
+        return String.format("(%s)-[:RELATED {label: '%s'}]->(%s)", sourceLabel, relationLabel, targetLabel);
+    }
 }
